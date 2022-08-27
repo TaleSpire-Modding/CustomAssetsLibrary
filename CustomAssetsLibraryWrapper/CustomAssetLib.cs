72c553cef3c407d3a32921a183bbac156ddda9fe
brajm008 <ujkkju117@outlook.com>
2022-08-28 02:11:33 +0930

    finished with wrapper.

--- CustomAssetsLibraryWrapper/CustomAssetLib.cs
+++ CustomAssetsLibraryWrapper/CustomAssetLib.cs
@@ -1,109 +1,27 @@
-﻿using System.IO;
-using BepInEx;
-using BepInEx.Configuration;
+﻿using BepInEx;
 using Bounce.Unmanaged;
-using CustomAssetsLibrary.DTO;
-using CustomAssetsLibrary.Tests;
-using CustomAssetsLibrary.Patches;
-using HarmonyLib;
 using LordAshes;
-using Unity.Entities;
-using Unity.Entities.Serialization;
-using UnityEngine;
 
 namespace CustomAssetsLibrary
 {
-    public enum LogLevel
-    {
-        None,
-        Low,
-        Medium,
-        High,
-        All,
-    }
-
     [BepInPlugin(Guid, Name, Version)]
     [BepInDependency(SmartConvert.Guid)]
     public class CustomAssetLib : BaseUnityPlugin
     {
         // constants
         public const string Guid = "org.PM.plugins.CustomAssetLib";
-        public const string Version = "1.1.1.0";
+        public const string Version = "1.1.1.1";
         private const string Name = "Plugin Masters' Custom Asset Library";
-
-        internal static ConfigEntry<bool> AutoClear { get; set; }
-        internal static ConfigEntry<bool> RunTestsConfig { get; set; }
-        internal static ConfigEntry<LogLevel> LogLevel { get; set; }
-        internal static Harmony harmony;
-
-        public static void DoPatching()
-        {
-            harmony = new Harmony(Guid);
-            harmony.PatchAll();
-            if (LogLevel.Value > CustomAssetsLibrary.LogLevel.None) Debug.Log($"Custom Asset Library Plugin: Patched.");
-        }
-
-        public static void UnPatch()
-        {
-            harmony.UnpatchSelf();
-            if (LogLevel.Value > CustomAssetsLibrary.LogLevel.None) Debug.Log($"Custom Asset Library Plugin: Unpatched.");
-        }
-
-        public static void RunTests()
-        {
-            // Can create / Serialize
-            new AssetLoaderTest().RunTests();
-            
-            new AtlasTest().RunTests();
-            new CreatureTest().RunTests();
-            new IndexTest().RunTests();
-            new MusicTest().RunTests();
-            new PlaceableTest().RunTests();
-
-            new AssetPackTest().RunTests();
-
-            new AssetDbPatchTest().RunTests();
-        }
-
-        public static void DoConfig(ConfigFile Config)
-        {
-            AutoClear = Config.Bind("Mini Loading", "Auto Clear Failed Minis", false);
-            LogLevel = Config.Bind("Logging", "Level", CustomAssetsLibrary.LogLevel.Low);
-            RunTestsConfig = Config.Bind("Tests", "Execute", false);
-            if (LogLevel.Value > CustomAssetsLibrary.LogLevel.None) Debug.Log($"Custom Asset Library Plugin: Config Bound.");
-        }
-
-        private void Awake()
-        {
-            DoConfig(Config);
-            DoPatching();
-            if (LogLevel.Value > CustomAssetsLibrary.LogLevel.None) Debug.Log($"Custom Asset Library Plugin:{Name} is Active."); 
-            // if (RunTestsConfig.Value) RunTests();
-        }
 
         // Public interface method to generate a binary INDEX for CAP
         public static void Generate(string directory)
-        {
-            var pack = new AssetPackContent();
-            pack.FromJson(directory);
-            if (LogLevel.Value > CustomAssetsLibrary.LogLevel.None) Debug.Log($"Added {Path.Combine(directory, "index")}");
-            WritePack(directory, pack);
-        }
-
-        public static void WritePack(string directory, AssetPackContent content)
-        { 
-            var blobref = content.GenerateBlobAssetReference();
-            var indexDestinationLocation = Path.Combine(directory, "index");
-            var writer = new StreamBinaryWriter(indexDestinationLocation);
-            writer.Write(blobref);
-        }
+            => CustomAssetsCompiler.TaleWeaverCompiler.Generate(directory);
 
         /// <summary>
         /// Create an NGuid from a string via MD5 Hash
         /// </summary>
         /// <param name="id">text to get hashed</param>
         /// <returns>NGuid of the string</returns>
-        public static NGuid GenerateID(string id)
-            => AssetDbOnSetupInternalsPatch.GenerateID(id);
+        public static NGuid GenerateID(string id) => CustomAssetsCompiler.TaleWeaverCompiler.GenerateID(id);
     }
 }
