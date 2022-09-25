1ed46033f774da9ef0fe7c69c8c89247243e528a
brajm008 <ujkkju117@outlook.com>
2022-09-26 02:35:08 +0930

    multi-threading wasn't working due to weird stream issue and jobs do not work with strings/complex types.

--- CustomAssetCompiler/TaleWeaverCompiler.cs
+++ CustomAssetCompiler/TaleWeaverCompiler.cs
@@ -1,13 +1,9 @@
 ﻿using System.IO;
 using System.Text;
-using System.Threading;
 using Bounce.Unmanaged;
 using CustomAssetsCompiler.CoreDTO;
-using Unity.Burst;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Entities.Serialization;
-using Unity.Jobs;
 using UnityEngine;
 using MD5 = System.Security.Cryptography.MD5;
 
@@ -17,52 +13,17 @@ namespace CustomAssetsCompiler
     {
         public const string Version = "1.0.0.0";
 
-        public enum impl
-        {
-            Sync,
-            Thread,
-            ThreadComplete,
-            Burst,
-            BurstComplete
-        }
+        // public enum impl
+        // {
+        //     Sync,
+        //     Thread,
+        //     ThreadComplete,
+        // }
 
         // Public interface method to generate a binary INDEX for CAP
-        public static void Generate(string directory, impl burst = impl.Sync, LogLevel logLevel = LogLevel.None)
+        public static void Generate(string directory, LogLevel logLevel = LogLevel.None)
         {
-            switch (burst)
-            {
-                case impl.Sync:
-                    WritePack(directory, logLevel);
-                    break;
-                case impl.Burst:
-                    new Packer
-                    {
-                        directory = directory,
-                        logLevel = logLevel
-                    }.Schedule();
-                    break;
-                case impl.BurstComplete:
-                    new Packer
-                    {
-                        directory = directory,
-                        logLevel = logLevel
-                    }.Schedule().Complete();
-                    break;
-                case impl.Thread:
-                    new Thread(() =>
-                    {
-                        WritePack(directory,logLevel);
-                    }).Start();
-                    break;
-                case impl.ThreadComplete:
-                    var t = new Thread(() =>
-                    {
-                        WritePack(directory, logLevel);
-                    });
-                    t.Start();
-                    t.Join();
-                    break;
-            }
+            WritePack(directory, logLevel);
         }
 
         public static void WritePack(string directory, LogLevel logLevel = LogLevel.None)
@@ -70,42 +31,21 @@ namespace CustomAssetsCompiler
             var content = new AssetPackContent();
             content.FromJson(directory);
             if (logLevel > LogLevel.None) Debug.Log($"Added {Path.Combine(directory, "index")}");
-            
+
             var blobref = content.GenerateBlobAssetReference();
             var indexDestinationLocation = Path.Combine(directory, "index");
             var writer = new StreamBinaryWriter(indexDestinationLocation);
             writer.Write(blobref);
+            writer.Dispose();
             File.WriteAllText(Path.Combine(directory, "assetpack.id"), $"{content.assetPackString},{content.Name}");
         }
 
-        [BurstCompile]
-        internal struct Packer : IJob
-        {
-            [ReadOnly]
-            internal string directory;
-
-            [ReadOnly]
-            internal LogLevel logLevel;
-            public void Execute()
-            {
-                var content = new AssetPackContent();
-                content.FromJson(directory);
-                if (logLevel > LogLevel.None) Debug.Log($"Added {Path.Combine(directory, "index")}");
-
-                var blobref = content.GenerateBlobAssetReference();
-                var indexDestinationLocation = Path.Combine(directory, "index");
-                var writer = new StreamBinaryWriter(indexDestinationLocation);
-                writer.Write(blobref);
-                File.WriteAllText(Path.Combine(directory, "assetpack.id"), $"{content.assetPackString},{content.Name}");
-            }
-        }
-
         /// <summary>
-            /// Create an NGuid from a string via MD5 Hash
-            /// </summary>
-            /// <param name="id">text to get hashed</param>
-            /// <returns>NGuid of the string</returns>
-            public static NGuid GenerateID(string id)
+        /// Create an NGuid from a string via MD5 Hash
+        /// </summary>
+        /// <param name="id">text to get hashed</param>
+        /// <returns>NGuid of the string</returns>
+        public static NGuid GenerateID(string id)
             => new NGuid(System.Guid.Parse(CreateMD5(id)));
 
         private static string CreateMD5(string input)
